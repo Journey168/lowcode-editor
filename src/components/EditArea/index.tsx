6aec31dcad74630516650fabc66f883683dc710d
Journey168 <1274049755@qq.com>
2024-10-17 17:20:44 +0800

    feat:根据配置映射物料组件渲染

--- src/components/EditArea/index.tsx
+++ src/components/EditArea/index.tsx
@@ -1,8 +1,10 @@
-import { useEffect } from "react";
-import { useComponetsStore } from "../../stores/components";
+import React, { useEffect, ReactNode } from "react";
+import { useComponentConfigStore } from "../../stores/component-config";
+import { useComponetsStore, Component } from "../../stores/components";
 
 export default function EditArea() {
   const { components, addComponent } = useComponetsStore();
+  const { componentConfig } = useComponentConfigStore();
 
   useEffect(() => {
     addComponent(
@@ -14,11 +16,43 @@ export default function EditArea() {
       },
       1
     );
+
+    addComponent(
+      {
+        id: 333,
+        name: "Button",
+        props: {
+          text: "无敌",
+        },
+        children: [],
+      },
+      222
+    );
   }, []);
 
+  function renderComponent(components: Component[]): ReactNode {
+    return components.map((component: Component) => {
+      const config = componentConfig?.[component.name];
+
+      if (!config.component) return null;
+
+      return React.createElement(
+        config.component,
+        {
+          key: component.id,
+          ...config.defaultProps,
+          ...component.props,
+        },
+        renderComponent(component.children || [])
+      );
+    });
+  }
+
   return (
-    <div>
-      <pre>{JSON.stringify(components, null, 2)}</pre>
+    <div className="h-[100%]">
+      {/* <pre>{JSON.stringify(components, null, 2)}</pre> */}
+
+      {renderComponent(components)}
     </div>
   );
 }
